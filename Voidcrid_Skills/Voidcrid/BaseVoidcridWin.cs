37662bf17e5e0c754fcdb588d6e84ebafd733984
sixfears7 <57415489+6fears7@users.noreply.github.com>
2022-12-23 12:18:18 -0500

    For review

--- Voidcrid_Skills/Voidcrid/BaseVoidcridWin.cs
+++ Voidcrid_Skills/Voidcrid/BaseVoidcridWin.cs
@@ -1,28 +1,77 @@
-// using RoR2;
-// using RoR2.Achievements;
-// using Voidcrid;
-// using R2API;
+using RoR2;
+using RoR2.Achievements;
 
-// namespace Voidcrid.Achievements {
-// 	public class VoidcridWin : BaseAchievement
-// {
+namespace Voidcrid.Achievements {
+
+    
+	public class VoidcridWin : BaseAchievement
+{
+
+        public override BodyIndex LookUpRequiredBodyIndex()
+        {
+            return BodyCatalog.FindBodyIndex("CrocoBody");
+        }
+
+        public override void OnBodyRequirementMet() {
+            base.OnBodyRequirementMet();
+
+            base.SetServerTracked(true);
+        }
+
+        public override void OnBodyRequirementBroken() {
+            base.SetServerTracked(false);
+
+            base.OnBodyRequirementBroken();
+        }
        
-//     [RegisterAchievement("VoidcridUnlock", "Skins.Croco.Voidcrid", null, null)]
-//     public class VoidcridUnlockAcievement : BaseEndingAchievement
-//     {
+    [RegisterAchievement("VoidcridUnlock", "Skins.Croco.Voidcrid", null,  typeof(VoidcridServerUnlock))]
+    
+        private class VoidcridServerUnlock : BaseServerAchievement
+        {
 
-//         public override BodyIndex LookUpRequiredBodyIndex()
-//         {
-//             return BodyCatalog.FindBodyIndex("CrocoBody");
-//         }
-//         public override bool ShouldGrant(RunReport runReport)
-//         {
-//             if (runReport.gameEnding && runReport.gameEnding.isWin && runReport.gameEnding == RoR2Content.GameEndings.MainEnding && this.localUser.cachedBody.bodyIndex == this.requiredBodyIndex)
-//             {
-//                 return true;
-//             }
-//             return false;
-//         }
-//     }
-// }
-// }
+            
+                protected virtual BodyIndex newtBoiIndex => BodyCatalog.FindBodyIndex("NewtMonster");
+                private int _grandfather = 0;
+                protected virtual int newtRequirement => 1;
+
+              private void onCharacterDeathGlobal(DamageReport damageReport) {
+
+                bool newtboi = damageReport.victimBody && damageReport.victimBodyIndex == newtBoiIndex;
+    
+                if (newtboi) {
+                    this._grandfather++;
+                    if (this._grandfather >= newtRequirement) {
+                        base.Grant();
+                    }
+                }
+            }
+			public override void OnInstall()
+			{
+				base.OnInstall();
+                GlobalEventManager.onCharacterDeathGlobal += onCharacterDeathGlobal;
+
+			}
+                    public override void OnUninstall() {
+                base.OnUninstall();
+
+                GlobalEventManager.onCharacterDeathGlobal -= onCharacterDeathGlobal;
+            }
+        }
+
+
+
+
+        // public override bool ShouldGrant(RunReport runReport)
+        // {
+        //     if (onCharacterDeathGlobal = 1 && this.localUser.cachedBody.bodyIndex == this.requiredBodyIndex)
+        //     {
+        //         base.Grant();
+        //         return true;
+        //     }
+
+
+        //     return false;
+        // }
+    }
+
+}
--- Voidcrid_Skills/Voidcrid/VoidcridDef.cs
+++ Voidcrid_Skills/Voidcrid/VoidcridDef.cs
@@ -9,6 +9,7 @@ using UnityEngine.AddressableAssets;
 using System.Reflection;
 using BepInEx.Configuration;
 using System.Runtime.CompilerServices;
+using Voidcrid.Achievements;
 
 
 
@@ -70,10 +71,10 @@ namespace Voidcrid
         private const string csProjName = "Voidcrid";
 
 
-      
+        private static UnlockableDef survivorUnlock;
+        public const string characterOutro = "..and so it left, a shell of its former self.";
+        public const string characterOutroFailure = "..and so it stayed, forever chained to the Abyss.";
         public void Awake()
-
-        
 
         
         {
@@ -232,6 +233,7 @@ namespace Voidcrid
                     
                 
             LoadAssetBundle();
+            CreateUnlockableDef();
 
             if (BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("com.DestroyedClone.AncientScepter"))
             {
@@ -252,8 +254,7 @@ namespace Voidcrid
             // };
 
 
-            LanguageAPI.Add("VOIDCRID_SKIN", "Voidcrid");
-            //We use LanguageAPI to add strings to the game, in the form of tokens
+            // LanguageAPI.Add("VOIDCRID_SKIN", "Voidcrid");
             LanguageAPI.Add("VOIDCRID_FLAMEBREATH", "Flamebreath");
             LanguageAPI.Add("VOIDCRID_FLAMEBREATH_DESC", $"<style=cDeath>Igniting.</style> <style=cIsDamage>Agile.</style> Release a burst of <style=cIsDamage>flame</style>, <style=cDeath>burning</style> enemies for <style=cIsDamage>250%</style> damage.");
             LanguageAPI.Add("VOIDCRID_NULLBEAM", $"<style=cArtifact>「N?ll Beam』</style>");
@@ -277,8 +278,16 @@ namespace Voidcrid
             LanguageAPI.Add("VOIDCRID_PASSIVE", "<style=cArtifact>Void</style>crid");
             LanguageAPI.Add("VOIDCRID_PASSIVE_DESC", "All <style=cArtifact>Void</style> attacks have a chance to <style=cArtifact>jail</style> enemies.");
             
-            
+            LanguageAPI.Add("ACHIEVEMENT_VOIDCRIDUNLOCK_NAME" , "Grandfather Paradox");
+	        LanguageAPI.Add("ACHIEVEMENT_VOIDCRIDUNLOCK_DESCRIPTION", "You monster.");
 
+            if (VoidcridPassiveShow.Value == true) {
+
+            LanguageAPI.Add("VOIDCRID_OUTRO_FLAVOR", characterOutro);
+            LanguageAPI.Add("VOIDCRID_OUTRO_FAILURE", characterOutroFailure);
+            }
+            
+            
             SkillDef voidBreath = ScriptableObject.CreateInstance<SkillDef>();
             SkillDef voidBeam = ScriptableObject.CreateInstance<SkillDef>();
             SkillDef voidEscape = ScriptableObject.CreateInstance<SkillDef>();
@@ -518,140 +527,25 @@ namespace Voidcrid
             }
 
         }
+    
+            private static UnlockableDef CreateUnlockableDef()
+        {
+            if (!survivorUnlock)
+            {
+                survivorUnlock = ScriptableObject.CreateInstance<UnlockableDef>();
+                survivorUnlock.cachedName = "Skins.Croco.Voidcrid";
+                survivorUnlock.nameToken = "ACHIEVEMENT_VOIDCRIDUNLOCK_NAME";
+                survivorUnlock.achievementIcon = mainAssetBundle.LoadAsset<Sprite>("deeprotentropy.png");
+                ContentAddition.AddUnlockableDef(survivorUnlock);
+            }
+
+            // if (Config.forceUnlock) return null;
+            return survivorUnlock;
+        }
     }
+
 }
 
         
-        //   private void AddVoidcridSkin()
-        // {
 
-
-
-        //     var bodyName = "CrocoBody";
-
-
-        //     var bodyPrefab = BodyCatalog.FindBodyPrefab(bodyName);
-        //     //Getting necessary components
-        //     var renderers = bodyPrefab.GetComponentsInChildren<Renderer>(true);
-        //     var skinController = bodyPrefab.GetComponentInChildren<ModelSkinController>();
-        //     var mdl = skinController.gameObject;
-
-        //     var blarg = new LoadoutAPI.SkinDefInfo
-        //     {
-        //         //Icon for your skin in the game, it can be any image, or you can use `LoadoutAPI.CreateSkinIcon` to easily create an icon that looks similar to the icons in the game.
-        //         Icon = LoadoutAPI.CreateSkinIcon(Color.black, Color.magenta, Color.red, Color.magenta),
-        //         //Replace `LumberJackCommando` with your skin name that can be used to access it through the code
-        //         Name = "Voidcrid",
-        //         //Replace `LUMBERJACK_SKIN` with your token
-        //         NameToken = "VOIDCRID_SKIN",
-        //         RootObject = mdl,
-                
-        //         BaseSkins = new SkinDef[] { skinController.skins[0] },
-
-        //         UnlockableDef = VoidcridSkinDef,
-
-  
-        //         GameObjectActivations = new SkinDef.GameObjectActivation[0],
-        //         //This is used to define which material should be used on a specific renderer.
-        //         //Only one material per renderer(mesh) is can be used
-        //         RendererInfos = new CharacterModel.RendererInfo[]
-        //         {
-        //             //To add another material replacement simply copy past this block right after and add `,` after the first one.
-                   
-        //             new CharacterModel.RendererInfo
-        //             {
-        //                 //Loading material from AssetBundle replace "@SkinTest:Assets/Resources/matLumberJack.mat" with your value.
-        //                 //It should be in this format `{provider name}:{path to asset in unity}`
-        //                 //To get path to asset you can right click on your asset in Unity and select `Copy path` option
-        //                 defaultMaterial = mainAssetBundle.LoadAsset<Material>("pinkvoid.mat"),
-
-        //                 //Should mesh cast shadows
-        //                 defaultShadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On,
-        //                 //Should mesh be ignored by overlays. For example shield outline from `Personal Shield Generator`
-        //                 ignoreOverlays = false,
-                      
-        //                 renderer = renderers[2]
-        //             },
-
-        //                   new CharacterModel.RendererInfo
-        //             {
-                       
-        //                 defaultMaterial = mainAssetBundle.LoadAsset<Material>("pink.mat"),
-
-        //                 //Should mesh cast shadows
-        //                 defaultShadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On,
-        //                 //Should mesh be ignored by overlays. For example shield outline from `Personal Shield Generator`
-        //                 ignoreOverlays = false,
-                      
-        //                 renderer = renderers[3]
-        //             }
-                    
-        //         },
-                
-
-                
-        //         // This is used to define which mesh should be used on a specific renderer.
-        //         MeshReplacements = new SkinDef.MeshReplacement[]
-        //         {
-        //             //To add another mesh replacement simply copy past this block right after and add `,` after the first one.
-        //             new SkinDef.MeshReplacement
-        //             {
-        //                 //Loading mesh from AssetBundle look at material replacement commentary to learn about how value should be changed.
-        //                 mesh = mainAssetBundle.LoadAsset<Mesh>("FINALLYFXD.mesh"),
-        //                 //Index you need can be found here: https://github.com/risk-of-thunder/R2Wiki/wiki/Creating-skin-for-vanilla-characters-with-custom-model#renderers
-        //                 renderer = renderers[2]
-        //             }
-        //         },
-        //         //You probably don't need to touch this line
-        //         ProjectileGhostReplacements = new SkinDef.ProjectileGhostReplacement[0],
-        //         //This is used to add skins for minions e.g. EngiTurrets
-        //         MinionSkinReplacements = new SkinDef.MinionSkinReplacement[0],
-        //     };
-
-        
-        
-
-    //         //Adding new skin to a character's skin controller
-    //         Array.Resize(ref skinController.skins, skinController.skins.Length + 1);
-    //         skinController.skins[skinController.skins.Length - 1] = LoadoutAPI.CreateNewSkinDef(blarg);
-
-    //         //Adding new skin into BodyCatalog
-
-    //         var skinsField = typeof(BodyCatalog).GetFieldValue<SkinDef[][]>("skins");
-    //         skinsField[(int) BodyCatalog.FindBodyIndex(bodyPrefab)] = skinController.skins;
-
-    // }
-
-
-    // public abstract class VoidcridUnlockable : ModdedUnlockable
-    // {
-    //     public abstract string AchievementTokenPrefix { get; }
-    //     public abstract string AchievementSpriteName { get; }
-
-    //     public override string AchievementIdentifier { get => "ACHIEVEMENT_VOIDCRIDCLEAR_NAME"; }
-    //     public override string UnlockableIdentifier { get => "ACHIEVEMENT_VOIDCRIDCLEAR_NAME"; }
-    //     public override string AchievementNameToken { get => "ACHIEVEMENT_VOIDCRIDCLEAR_NAME"; }
-    //     public override string AchievementDescToken { get =>"ACHIEVEMENT_VOIDCRIDCLEAR_DESC" ;}
-    //     public override string UnlockableNameToken { get => "ACHIEVEMENT_VOIDCRIDCLEAR_NAME" ;}
-
-    //     public override Sprite Sprite => VoidcridDef.mainAssetBundle.LoadAsset<Sprite>("voidcrid.png");
-
-    //     public override Func<string> GetHowToUnlock
-    //     {
-    //         get => () => Language.GetStringFormatted("ACHIEVEMENT_VOIDCRIDCLEAR_DESC", new object[]
-    //                         {
-    //                             Language.GetString(AchievementDescToken),
-    //                             Language.GetString(AchievementDescToken)
-    //                         });
-    //     }
-
-    //     public override Func<string> GetUnlocked
-    //     {
-    //         get => () => Language.GetStringFormatted("ACHIEVEMENT_VOIDCRIDCLEAR_NAME", new object[]
-    //                         {
-    //                             Language.GetString(AchievementNameToken),
-    //                             Language.GetString(AchievementDescToken)
-    //                         });
-    //     }
-    // }
 
